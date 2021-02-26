29616d51cba0a387778c0859081d85bbb4075511
sahana-alt <51981952+sahana-alt@users.noreply.github.com>
2021-02-26 18:26:50 +0530

    Delete tata directory

--- tata/Day1_Assignment_Sahana/Worker/src/main/java/com/tata/Daily.java
+++
@@ -1,8 +0,0 @@
-package com.tata;
-
-public class Daily extends Worker{
-    public void Pay(int h){
-        System.out.println(getSalrate()*h/24);
-    }
-
-}
--- tata/Day1_Assignment_Sahana/Worker/src/main/java/com/tata/Salaried.java
+++
@@ -1,8 +0,0 @@
-package com.tata;
-
-public class Salaried extends Worker {
-    public void Pay(int h){
-
-        System.out.println(getSalrate()*h);
-    }
-}
